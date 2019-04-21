a309d88f40d7e6db290af9ee9fac6e03f9d603a7
ymtwithcarp <kkcymt@yahoo.co.jp>
2019-04-21 16:53:33 +0900

    fix インデント修正

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,5 +1,5 @@
 class TradesController < ApplicationController
-	before_action :set_trade
+  before_action :set_trade
 
   def update
     Payjp.api_key = PAYJP_SECRET_KEY
@@ -11,25 +11,25 @@ class TradesController < ApplicationController
     @trade.first.save
   end
 
-	def index
-		@item = @trade.item
-		@buyer = @item.buyer
-		@seller = @item.seller
-		@tradecomment = Tradecomment.new
-		@tradecomments = Tradecomment.where(trade_id: @trade.id).order(created_at: "DESC")
-	end
+  def index
+    @item = @trade.item
+    @buyer = @item.buyer
+    @seller = @item.seller
+    @tradecomment = Tradecomment.new
+    @tradecomments = Tradecomment.where(trade_id: @trade.id).order(created_at: "DESC")
+  end
 
-	def create
-		@tradecomment = Tradecomment.create(comment_params)
-		redirect_to action: 'index'
-	end
+  def create
+    @tradecomment = Tradecomment.create(comment_params)
+    redirect_to action: 'index'
+  end
 
 private
-	def set_trade
-		@trade = Trade.find_by(item_id: params[:item_id])
-	end
+  def set_trade
+    @trade = Trade.find_by(item_id: params[:item_id])
+  end
 
-	def comment_params
-		params.require(:tradecomment).permit(:comment).merge(trade_id: @trade.id, user_id: current_user.id)
-	end
+  def comment_params
+    params.require(:tradecomment).permit(:comment).merge(trade_id: @trade.id, user_id: current_user.id)
+  end
 end
